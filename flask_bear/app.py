f42979f8c65df52281ecfdc3998c9a5b6a3d0326
Jay Xu <sgliv8@gmail.com>
2015-08-03 12:26:34 -0700

    Added gitigore file

---
+++ flask_bear/app.py
@@ -0,0 +1,40 @@
+import json
+from flask import (Flask, render_template, redirect, 
+					url_for, request, make_response,
+					flash)
+
+from options import DEFAULTS
+
+app = Flask(__name__)
+app.secret_key = '542q5knjeqr054l@#$102353fadnvxsdsfasf'
+
+def get_saved_data():
+	try:
+		data = json.loads(request.cookies.get('character'))
+	except TypeError:
+		data = {}
+	return data
+
+@app.route('/')
+def index():
+	data = get_saved_data()
+	return render_template('index.html', saves=data)
+
+
+@app.route('/builder')
+def builder():
+	return render_template('builder.html',
+							saves = get_saved_data(),
+							options = DEFAULTS)
+
+@app.route('/save', methods=['POST'])
+def save():
+	flash("Alright! That looks awesome!")
+	response = make_response(redirect(url_for('builder')))
+	data = get_saved_data()
+	data.update(dict(request.form.items()))
+	response.set_cookie('character', json.dumps(data))
+	return response
+
+
+app.run(debug=True, host='127.0.0.1', port=8000)
